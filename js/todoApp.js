63453dd50f01ab5ba009cef72dc6c54057aa3704
Linn Abrahamsson <linab708@student.liu.se>
2017-11-20 16:27:13 +0100

    final

--- js/todoApp.js
+++ js/todoApp.js
@@ -1,55 +1,90 @@
 /**
  * App for making a simple todo list
- * Author: 
+ * Author:
  *
  * @requires jQuery
  * @version 0.0.1
  */
-var TodoApp = ( function() {
+var TodoApp = (function () {
     // Properties
     var i;
-    var list = $('#list');
+    var $list = $('#list'),
+        $addButton = $('#addButton'),
+        $saveButton = $('#saveButton'),
+        $clearButton = $('#clearButton'),
+        $inputField = $('#inputField');
     var todoIds = [];
 
     // Methods
-
-
     function removeRow(event) {
-        console.log(event.data.todoId);
-        $('#'+event.data.todoId).remove();
+        var todoId = event.data.todoId;
+        $('#' + todoId).remove();
+        localStorage.removeItem(todoId);
+        var index = todoIds.indexOf(todoId);
+        todoIds.splice(index, 1);
+    }
+
+    function addTodo(todo='  ', id='0') {
+        var inputTodo = ($inputField.val() ? $inputField.val() : todo);
+        if (id==0) {
+            i++;
+            id = 'todo-' + i;
+            todoIds.push(id);
+        }
+        var newTodo = $('<li></li>', {
+            id: id
+        });
+        $('<span></span>', {
+            contenteditable: "true"
+        }).html(inputTodo).appendTo(newTodo);
+        $('<a>&times;</a>').on('click', {todoId: id}, removeRow).appendTo(newTodo);
+        $list.append(newTodo);
+        $inputField.val('');
+    }
+
+    function saveList() {
+        localStorage.setItem('todoIds', JSON.stringify(todoIds));
+        localStorage.setItem('i', i);
+        todoIds.forEach(function (todoId) {
+            localStorage.setItem(todoId, $('#'+todoId).find('span').html())
+        });
+    }
+
+    function loadList() {
+        i = localStorage.getItem('i');
+        todoIds = JSON.parse(localStorage.getItem('todoIds'));
+        todoIds.forEach(function(id) {
+            addTodo(localStorage.getItem(id),id);
+        })
+    }
+
+    function clearList() {
+        todoIds.forEach(function(id) {
+            $('#'+id).find('span').html("   ");
+        });
+        localStorage.clear();
     }
 
     function init() {
         // Application init code
-        // The application is initialized with 12 "empty to-dos"
-        i = 0;
-        for (j=0; j<12; j++) {
-            i++;
-            var emptyTodo = $('<li></li>', {
-                id: "todo-" + i
-            });
-            $('<a>&times;</a>', {
-                href: "#"
-            }).appendTo(emptyTodo);
-            $('<span></span>').appendTo(emptyTodo);
-            $(list).append(emptyTodo);
-            todoIds.push("todo-"+i);
+        // The application is initialized with 5 "empty to-dos"
+        if(localStorage.getItem('todoIds')) {
+            loadList();
+        } else {
+            i = 0;
+            for (j = 0; j < 5; j++) {
+                addTodo();
+            }
         }
-
-
-        $(function(){
-            todoIds.forEach(function(todoId){
-                $('#'+todoId).find("a").click({todoId: todoId}, removeRow);
-            });
-        });
-
-
-
+        $addButton.on('click', addTodo);
+        $saveButton.on('click', saveList);
+        $clearButton.on('click', clearList);
     }
+
     return {
-        init : init
+        init: init
     };
-  
-} )();
+
+})();
 
 TodoApp.init(); // Run application
